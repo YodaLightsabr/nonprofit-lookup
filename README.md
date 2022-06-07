6ed8378f3c5e046025ff8555d3392f0d1aee9ecd
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-06-07 12:07:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# nonprofit-lookup
+`https://nonprofit.yodacode.xyz/api?org=<org_name>&state=<state>&type=<type>`
+
+`<org_name>` - The organization to search
+`<state>` - A two letter state abbreviation
+`<type>` - The type of form. Options:
+  - `A`: Only 990
+  - `B`: Only 990-PF
+  - `undefined` or blank: Both
---
+++ api/index.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+import tabletojson from 'tabletojson';
+
+const convertTable = tabletojson.Tabletojson.convert;
+
+const tableSelector = `html body div#finder-content form#form1 div.finder-table-results div table#MainContent_GridView1`;
+const tableLinkSelector = `tr td a`;
+
+/**
+ * 
+ * @param {string} org Search query
+ * @param {string} state A two letter state abbreviation.
+ * @param {'A'|'B'|''|undefined} type The type of record to find. A, B, or blank. 990 or 990-PF
+ * @returns 
+ */
+const generateURL = (org, state, type = '') => `https://990finder.foundationcenter.org/990results.aspx?990_type=${encodeURIComponent(type)}&fn=${encodeURIComponent(org).split('%20').join('+')}&st=${encodeURIComponent(state)}&zp=&ei=&fy=&action=Search`;
+
+
+const lookup = async (org, state, type = '') => {
+    const url = generateURL(org, state, type);
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const links = [];
+    $(tableSelector + ' ' + tableLinkSelector).each((index, value) => links.push($(value).attr('href')));
+    const table = convertTable(`<table>${$(tableSelector).html()}</table>`)[0].map((row, i) => ({ ...row, Link: links[i] }));
+    return table;
+}
+
+export default async function (req, res) {
+    const data = await lookup(req.query.org, req.query.state, req.query.type);
+    res.json(data);
+}
